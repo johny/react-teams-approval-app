f51e94051f8caabe85c60c9b5bad62c789b50ffd
Jan Kraus <kraus.jan@gmail.com>
2020-04-03 20:16:05 +0100

    Setup App skeleton with Redux and Redux Toolkit

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux'
 
 import './index.css';
 
-import App from './App';
+import App from './app/App'
+import store from './app/store'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+const render = () => {
+  const root = document.getElementById('root')
+  ReactDOM.render(
+    <React.StrictMode>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </React.StrictMode>, root);
+}
+
+render()
