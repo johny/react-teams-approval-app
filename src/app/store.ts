353eaceda775b19c374d6f874f1a90f7d2986ad8
Jan Kraus <kraus.jan@gmail.com>
2020-04-06 00:52:57 +0100

    Sync store with localStorage API

--- src/app/store.ts
+++ src/app/store.ts
@@ -2,12 +2,18 @@ import { configureStore, Action } from '@reduxjs/toolkit'
 import { ThunkAction } from 'redux-thunk'
 
 import rootReducer, { RootState } from './rootReducer'
+import { saveState, loadState } from '../api/localStorage'
 
 const store = configureStore({
-  reducer: rootReducer
+  reducer: rootReducer,
+  preloadedState: loadState()
 })
 
 export type AppDispatch = typeof store.dispatch
 export type AppThunk = ThunkAction<void, RootState, unknown, Action<string>>
+
+store.subscribe(() => {
+  saveState(store.getState())
+})
 
 export default store
