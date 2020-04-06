3daaa11a1ab8aa5b24e00b1a00670bf60d3cccc2
Jan Kraus <kraus.jan@gmail.com>
2020-04-06 10:17:04 +0100

    Updates

---
+++ precache-manifest.9fe46d6d39f96a6f0b74969bb2fba8f0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4ce649a49f3a9f142c6287830bd7e7fc",
+    "url": "/react-teams-approval-app/index.html"
+  },
+  {
+    "revision": "b8988ec8c151b72ada61",
+    "url": "/react-teams-approval-app/static/css/main.5cbb1563.chunk.css"
+  },
+  {
+    "revision": "0cbfb0446472f5dac2b7",
+    "url": "/react-teams-approval-app/static/js/2.33b5623c.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-teams-approval-app/static/js/2.33b5623c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b8988ec8c151b72ada61",
+    "url": "/react-teams-approval-app/static/js/main.79ae6ff0.chunk.js"
+  },
+  {
+    "revision": "b969c6c164ae2e6c29e4",
+    "url": "/react-teams-approval-app/static/js/runtime-main.0748309f.js"
+  }
+]);
